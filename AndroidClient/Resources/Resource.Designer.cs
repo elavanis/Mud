bd57e76f9fabbfef12b15af08cacf415e4eb59f3
Tom Johnson <tom.johnson@zirmed.com>
2018-08-08 15:01:08 -0400

    working android icon

--- AndroidClient/Resources/Resource.Designer.cs
+++ AndroidClient/Resources/Resource.Designer.cs
@@ -3150,6 +3150,9 @@ namespace AndroidClient
 			// aapt resource value: 0x7f030002
 			public const int ic_launcher_round = 2130903042;
 			
+			// aapt resource value: 0x7f030003
+			public const int icon = 2130903043;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
