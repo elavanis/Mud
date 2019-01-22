76a83b18d0ab24ac4e4fd637ba62cfa4ea32770d
Tom Johnson <tom.johnson@zirmed.com>
2019-01-22 15:56:13 -0500

    another unit test

--- UnitTests/ObjectsUnitTest/Command/PC/PutUnitTest.cs
+++ UnitTests/ObjectsUnitTest/Command/PC/PutUnitTest.cs
@@ -3,8 +3,11 @@ using Moq;
 using Objects.Command.Interface;
 using Objects.Command.PC;
 using Objects.Global;
+using Objects.Global.Engine.Engines.Interface;
+using Objects.Global.Engine.Interface;
 using Objects.Global.FindObjects.Interface;
 using Objects.Item.Interface;
+using Objects.Item.Items.Interface;
 using Objects.Mob.Interface;
 using Objects.Room.Interface;
 using Shared.TagWrapper.Interface;
@@ -28,6 +31,12 @@ namespace ObjectsUnitTest.Command.PC
         Mock<IFindObjects> findObjects;
         Mock<IItem> item;
         Mock<IRoom> room;
+        Mock<IContainer> container;
+        Mock<IItem> itemContainer;
+        Mock<IEngine> engine;
+        Mock<IEvent> eventEngine;
+        List<IItem> containerItems;
+        List<IItem> performerItems;
 
         [TestInitialize]
         public void Setup()
@@ -40,16 +49,30 @@ namespace ObjectsUnitTest.Command.PC
             findObjects = new Mock<IFindObjects>();
             item = new Mock<IItem>();
             room = new Mock<IRoom>();
+            container = new Mock<IContainer>();
+            itemContainer = container.As<IItem>();
+            engine = new Mock<IEngine>();
+            eventEngine = new Mock<IEvent>();
+            containerItems = new List<IItem>();
+            performerItems = new List<IItem>();
 
             tagWrapper.Setup(e => e.WrapInTag(It.IsAny<string>(), TagType.Info)).Returns((string x, TagType y) => (x));
             mockCommand.Setup(e => e.Parameters).Returns(new List<IParameter>());
             itemParameter.Setup(e => e.ParameterValue).Returns("parmValue");
             containerParameter.Setup(e => e.ParameterValue).Returns("parmValue2");
             findObjects.Setup(e => e.FindHeldItemsOnMob(performer.Object, itemParameter.Object.ParameterValue)).Returns(new List<IItem>() { item.Object });
-            findObjects.Setup(e => e.FindItemsInRoom(room.Object, containerParameter.Object.ParameterValue)).Returns(new List<IItem>() { item.Object });
+            findObjects.Setup(e => e.FindItemsInRoom(room.Object, containerParameter.Object.ParameterValue)).Returns(new List<IItem>() { itemContainer.Object });
+            performer.Setup(e => e.Room).Returns(room.Object);
+            performer.Setup(e => e.Items).Returns(performerItems);
+            engine.Setup(e => e.Event).Returns(eventEngine.Object);
+            container.Setup(e => e.Items).Returns(containerItems);
+            performerItems.Add(item.Object);
+            item.Setup(e => e.SentenceDescription).Returns("item");
+            itemContainer.Setup(e => e.SentenceDescription).Returns("container");
 
             GlobalReference.GlobalValues.TagWrapper = tagWrapper.Object;
             GlobalReference.GlobalValues.FindObjects = findObjects.Object;
+            GlobalReference.GlobalValues.Engine = engine.Object;
 
             command = new Put();
         }
@@ -100,8 +123,10 @@ namespace ObjectsUnitTest.Command.PC
             mockCommand.Setup(e => e.Parameters).Returns(new List<IParameter>() { itemParameter.Object, containerParameter.Object });
 
             IResult result = command.PerformCommand(performer.Object, mockCommand.Object);
-            Assert.IsTrue(result.AllowAnotherCommand);
-            Assert.AreEqual("Put [Item Name] [Container]", result.ResultMessage);
+            Assert.IsFalse(result.AllowAnotherCommand);
+            Assert.AreEqual("You put item in container.", result.ResultMessage);
+            Assert.IsFalse(performerItems.Contains(item.Object));
+            Assert.IsTrue(containerItems.Contains(item.Object));
         }
     }
 }
