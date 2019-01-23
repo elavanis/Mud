cae740291411efc104c513a70853d4eebb4d48a4
Tom Johnson <tom.johnson@zirmed.com>
2019-01-23 13:58:43 -0500

    more unit tests

--- UnitTests/ObjectsUnitTest/Magic/Enchantment/PutEnchantmentUnitTest.cs
+++ UnitTests/ObjectsUnitTest/Magic/Enchantment/PutEnchantmentUnitTest.cs
@@ -1,4 +1,13 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Objects.Effect.Interface;
+using Objects.Global;
+using Objects.Global.Random.Interface;
+using Objects.Item.Interface;
+using Objects.Item.Items.Interface;
+using Objects.Magic.Enchantment;
+using Objects.Mob.Interface;
+using Objects.Room.Interface;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,10 +17,47 @@ namespace ObjectsUnitTest.Magic.Enchantment
     [TestClass]
     public class PutEnchantmentUnitTest
     {
-        [TestMethod]
-        public void WritePutEnchantmentUnitTests()
+        PutEnchantment enchantment;
+        Mock<IRandom> random;
+        Mock<IMobileObject> performer;
+        Mock<IItem> item;
+        Mock<IContainer> container;
+        Mock<IEffectParameter> effectParameter;
+        Mock<IRoom> room;
+        Mock<IEffect> effect;
+
+        [TestInitialize]
+        public void Setup()
         {
-            Assert.IsTrue(false);
+            random = new Mock<IRandom>();
+            performer = new Mock<IMobileObject>();
+            item = new Mock<IItem>();
+            container = new Mock<IContainer>();
+            effectParameter = new Mock<IEffectParameter>();
+            room = new Mock<IRoom>();
+            effect = new Mock<IEffect>();
+            enchantment = new PutEnchantment();
+
+            enchantment.ActivationPercent = 100;
+            enchantment.Parameter = effectParameter.Object;
+            enchantment.Effect = effect.Object;
+            random.Setup(e => e.PercentDiceRoll(100)).Returns(true);
+            performer.Setup(e => e.Room).Returns(room.Object);
+
+            GlobalReference.GlobalValues.Random = random.Object;
+        }
+
+        [TestMethod]
+        public void PutEnchantment_Put()
+        {
+            enchantment.Put(performer.Object, item.Object, container.Object);
+
+            effectParameter.VerifySet(e => e.ObjectRoom = room.Object);
+            effectParameter.VerifySet(e => e.Item = item.Object);
+            effectParameter.VerifySet(e => e.Target = performer.Object);
+            effectParameter.VerifySet(e => e.Container = container.Object);
+
+            effect.Verify(e => e.ProcessEffect(effectParameter.Object), Times.Once);
         }
     }
 }
