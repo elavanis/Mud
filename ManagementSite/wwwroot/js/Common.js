a91a5337b7822e68a940646f98563c30abea6f7d
Tom Johnson <tom.johnson@zirmed.com>
2019-02-21 15:01:24 -0500

    got working site test stat

--- ManagementSite/wwwroot/js/Common.js
+++ ManagementSite/wwwroot/js/Common.js
@@ -1,16 +1,14 @@
-﻿jQuery.extend({
-    getValues: function (url) {
-        var result = null;
-        $.ajax({
-            url: url,
-            type: 'get',
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            async: false,
-            success: function (data) {
-                result = data;
-            }
-        });
-        return result;
-    }
-});
+﻿function getValues(url) {
+    var result = [];
+    $.ajax({
+        url: url,
+        type: 'get',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        async: false,
+        success: function (data) {
+            result = data;
+        }
+    });
+    return result;
+}
