906cdeb6e97bfc2bf4596b23344df08f602a637f
Tom Johnson <tom.johnson@zirmed.com>
2019-02-21 15:37:14 -0500

    finishing changes

--- ManagementSite/BL/StatsReader.cs
+++ ManagementSite/BL/StatsReader.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Objects.Global.Serialization;
+using Objects.Global.Serialization.Interface;
+using Shared.PerformanceCounters.Interface;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +11,35 @@ namespace ManagementSite.BL
 {
     public static class StatsReader
     {
-        public static
+        private static object padLock = new object();
+        private static List<ICounters> stats = null;
+        private static DateTime lastUpdate = new DateTime();
+        private static ISerialization serialization = new JsonSerialization();
+
+        public static List<ICounters> Stats
+        {
+            get
+            {
+                lock (padLock)
+                {
+                    if (DateTime.Now.Subtract(lastUpdate).TotalSeconds > 60)
+                    {
+                        stats = ReloadStats();
+                        lastUpdate = DateTime.Now;
+                    }
+                }
+
+                return stats;
+            }
+        }
+
+        private static List<ICounters> ReloadStats()
+        {
+            List<string> files = Directory.GetFiles(@"C:\Mud\Stats", "*.*", SearchOption.AllDirectories).ToList();
+            files.Sort();
+            string fileContents = File.ReadAllText(files[0]);
+            List<ICounters> counters = serialization.Deserialize<List<ICounters>>(fileContents);
+            return counters;
+        }
     }
 }
