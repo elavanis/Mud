d76803d163286e27d0578a2dc0ed35af267bc6b9
Tom Johnson <tom.johnson@zirmed.com>
2019-02-21 15:01:24 -0500

    got working site test stat

---
+++ ManagementSite/BL/StatsReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementSite.BL
+{
+    public static class StatsReader
+    {
+        public static
+    }
+}
