d74056df8daca30b153c813a6b62254119fdf3ba
Tom Johnson <tom.johnson@zirmed.com>
2019-03-06 14:33:53 -0500

    add blank unit tests

---
+++ UnitTests/ObjectsUnitTest/Personality/Personalities/ElementalUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectsUnitTest.Personality.Personalities
+{
+    [TestClass]
+    public class ElementalUnitTest
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+
+        [TestMethod]
+        public void Elemental_WriteTests()
+        {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
