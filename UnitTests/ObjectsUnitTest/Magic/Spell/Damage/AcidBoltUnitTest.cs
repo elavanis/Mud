c8d12f912f5d535c2ec0ffe92265a66a5f3d6bb5
Tom Johnson <tom.johnson@zirmed.com>
2019-03-26 12:43:19 -0400

    update for default magic spells unit tests

---
+++ UnitTests/ObjectsUnitTest/Magic/Spell/Damage/AcidBoltUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Objects.Die.Interface;
+using Objects.Global;
+using Objects.Global.DefaultValues.Interface;
+using Objects.Magic.Spell.Damage;
+using Shared.TagWrapper.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Shared.TagWrapper.TagWrapper;
+
+namespace ObjectsUnitTest.Magic.Spell.Damage
+{
+    [TestClass]
+    public class AcidBoltUnitTest
+    {
+        AcidBolt acidBolt;
+        Mock<IDefaultValues> defaultValues;
+        Mock<IDice> dice;
+        Mock<ITagWrapper> tagWrapper;
+        [TestInitialize]
+        public void Setup()
+        {
+            defaultValues = new Mock<IDefaultValues>();
+            dice = new Mock<IDice>();
+            tagWrapper = new Mock<ITagWrapper>();
+
+            defaultValues.Setup(e => e.DiceForSpellLevel(20)).Returns(dice.Object);
+            tagWrapper.Setup(e => e.WrapInTag(It.IsAny<string>(), TagType.Info)).Returns((string x, TagType y) => (x));
+
+            GlobalReference.GlobalValues.DefaultValues = defaultValues.Object;
+            GlobalReference.GlobalValues.TagWrapper = tagWrapper.Object;
+
+            acidBolt = new AcidBolt();
+        }
+
+        [TestMethod]
+        public void AcidBolt()
+        {
+            Assert.AreEqual("Acid leaps from you hands and lands on {target} burning their skin.", acidBolt.PerformerNotificationSuccess.Message);
+            Assert.AreEqual("Acid leaps from {performer} arms and lands {target} burning their skin.", acidBolt.RoomNotificationSuccess.Message);
+            Assert.AreEqual("Acid leaps from {performer} arms and lands on you burning your skin.", acidBolt.TargetNotificationSuccess.Message);
+        }
+    }
+}
---
+++ UnitTests/ObjectsUnitTest/Magic/Spell/Damage/SmiteUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Objects.Die.Interface;
+using Objects.Global;
+using Objects.Global.DefaultValues.Interface;
+using Objects.Magic.Spell.Damage;
+using Shared.TagWrapper.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Shared.TagWrapper.TagWrapper;
+
+namespace ObjectsUnitTest.Magic.Spell.Damage
+{
+    [TestClass]
+    public class SmiteUnitTest
+    {
+        Smite smite;
+        Mock<IDefaultValues> defaultValues;
+        Mock<IDice> dice;
+        Mock<ITagWrapper> tagWrapper;
+        [TestInitialize]
+        public void Setup()
+        {
+            defaultValues = new Mock<IDefaultValues>();
+            dice = new Mock<IDice>();
+            tagWrapper = new Mock<ITagWrapper>();
+
+            defaultValues.Setup(e => e.DiceForSpellLevel(70)).Returns(dice.Object);
+            tagWrapper.Setup(e => e.WrapInTag(It.IsAny<string>(), TagType.Info)).Returns((string x, TagType y) => (x));
+
+            GlobalReference.GlobalValues.DefaultValues = defaultValues.Object;
+            GlobalReference.GlobalValues.TagWrapper = tagWrapper.Object;
+
+            smite = new Smite();
+        }
+
+        [TestMethod]
+        public void Smite()
+        {
+            Assert.AreEqual("You thrust both hands toward {target} and say {SpellName}.  A fireball leaps through the air and grows until it lands on {target}.", smite.PerformerNotificationSuccess.Message);
+            Assert.AreEqual("{performer} thrust both hands toward {target} and says {SpellName}.  A giant ball of fire flies through the air towards {target} engulfing them in fire.", smite.RoomNotificationSuccess.Message);
+            Assert.AreEqual("{performer} thrust both hands toward you and says {SpellName}.  A giant ball of fire flies through the air towards you.", smite.TargetNotificationSuccess.Message);
+        }
+    }
+}
