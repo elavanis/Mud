ccff41d693e436158f1026f000367f9e11a217f6
Tom Johnson <nolueram@gmail.com>
2019-04-16 05:31:25 -0400

    starting unit tests for party

--- UnitTests/ObjectsUnitTest/Command/PC/PartyUnitTest.cs
+++ UnitTests/ObjectsUnitTest/Command/PC/PartyUnitTest.cs
@@ -1,26 +1,56 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Objects.Command.Interface;
 using Objects.Command.PC;
+using Objects.Global;
+using Shared.TagWrapper.Interface;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using static Shared.TagWrapper.TagWrapper;
 
 namespace ObjectsUnitTest.Command.PC
 {
     [TestClass]
     public class PartyUnitTest
     {
-        Party party;
+        IMobileObjectCommand command;
+        Mock<ITagWrapper> tagWrapper;
 
         [TestInitialize]
         public void Setup()
         {
-            party = new Party();
+            tagWrapper = new Mock<ITagWrapper>();
+            tagWrapper.Setup(e => e.WrapInTag(It.IsAny<string>(), TagType.Info)).Returns((string x, TagType y) => (x));
+            GlobalReference.GlobalValues.TagWrapper = tagWrapper.Object;
+
+            command = new Party();
         }
 
         [TestMethod]
         public void Party_WritePartyUnitTests()
         {
             Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void Party_Instructions()
+        {
+            IResult result = command.Instructions;
+
+            Assert.IsTrue(result.AllowAnotherCommand);
+            Assert.AreEqual(@"Party Invite {Person To Invite}
+Party Decline
+Party {Message To Send To Party}", result.ResultMessage);
+        }
+
+        [TestMethod]
+        public void Party_CommandTrigger()
+        {
+            IEnumerable<string> result = command.CommandTrigger;
+            Assert.AreEqual(1, result.Count());
+            Assert.IsTrue(result.Contains("Party"));
         }
     }
 }
