db24a1d75422ad51c9a95150029aab80ac0ee818
Tom Johnson <nolueram@gmail.com>
2019-04-19 05:26:14 -0400

    game stats

--- UnitTests/ObjectsUnitTest/Command/World/GameStatsUnitTest.cs
+++ UnitTests/ObjectsUnitTest/Command/World/GameStatsUnitTest.cs
@@ -11,6 +11,8 @@ using Objects.Zone.Interface;
 using Objects.Room.Interface;
 using Objects.Mob.Interface;
 using Objects.Item.Interface;
+using Objects.GameDateTime.Interface;
+using Objects.Global.GameDateTime.Interface;
 
 namespace ObjectsUnitTest.Command.World
 {
@@ -19,6 +21,20 @@ namespace ObjectsUnitTest.Command.World
     {
 
         GameStats gameStats;
+        Mock<IUpTime> upTime;
+        Mock<ITickTimes> tickTimes;
+        Mock<IWorld> world;
+        Mock<IZone> zone1;
+        Mock<IZone> zone2;
+        Mock<IRoom> room;
+        Mock<INonPlayerCharacter> npc;
+        Mock<IPlayerCharacter> pc;
+        Mock<IItem> item;
+        Mock<IGameDateTime> gameDateTime;
+        Mock<IInGameDateTime> inGameDateTime;
+        Dictionary<int, IZone> dictionaryZones;
+        Dictionary<int, IRoom> dictionaryRoom1;
+        Dictionary<int, IRoom> dictionaryRoom2;
 
         [TestInitialize]
         public void Setup()
@@ -26,23 +42,21 @@ namespace ObjectsUnitTest.Command.World
             GlobalReference.GlobalValues = new GlobalValues();
 
             gameStats = new GameStats();
-        }
 
-        [TestMethod]
-        public void GameStats_GenerateGameStats()
-        {
-            Mock<IUpTime> upTime = new Mock<IUpTime>();
-            Mock<ITickTimes> tickTimes = new Mock<ITickTimes>();
-            Mock<IWorld> world = new Mock<IWorld>();
-            Mock<IZone> zone1 = new Mock<IZone>();
-            Mock<IZone> zone2 = new Mock<IZone>();
-            Mock<IRoom> room = new Mock<IRoom>();
-            Mock<INonPlayerCharacter> npc = new Mock<INonPlayerCharacter>();
-            Mock<IPlayerCharacter> pc = new Mock<IPlayerCharacter>();
-            Mock<IItem> item = new Mock<IItem>();
-            Dictionary<int, IZone> dictionaryZones = new Dictionary<int, IZone>();
-            Dictionary<int, IRoom> dictionaryRoom1 = new Dictionary<int, IRoom>();
-            Dictionary<int, IRoom> dictionaryRoom2 = new Dictionary<int, IRoom>();
+            upTime = new Mock<IUpTime>();
+            tickTimes = new Mock<ITickTimes>();
+            world = new Mock<IWorld>();
+            zone1 = new Mock<IZone>();
+            zone2 = new Mock<IZone>();
+            room = new Mock<IRoom>();
+            npc = new Mock<INonPlayerCharacter>();
+            pc = new Mock<IPlayerCharacter>();
+            item = new Mock<IItem>();
+            gameDateTime = new Mock<IGameDateTime>();
+            inGameDateTime = new Mock<IInGameDateTime>();
+            dictionaryZones = new Dictionary<int, IZone>();
+            dictionaryRoom1 = new Dictionary<int, IRoom>();
+            dictionaryRoom2 = new Dictionary<int, IRoom>();
 
             upTime.Setup(e => e.FormatedUpTime(It.IsAny<DateTime>())).Returns("uptime");
             tickTimes.Setup(e => e.Times).Returns("tickTimes");
@@ -63,11 +77,17 @@ namespace ObjectsUnitTest.Command.World
             room.Setup(e => e.NonPlayerCharacters).Returns(new List<INonPlayerCharacter>() { npc.Object });
             room.Setup(e => e.PlayerCharacters).Returns(new List<IPlayerCharacter>() { pc.Object });
             room.Setup(e => e.Items).Returns(new List<IItem>() { item.Object });
+            inGameDateTime.Setup(e => e.GameDateTime).Returns(gameDateTime.Object);
 
             GlobalReference.GlobalValues.UpTime = upTime.Object;
             GlobalReference.GlobalValues.TickTimes = tickTimes.Object;
             GlobalReference.GlobalValues.World = world.Object;
+            GlobalReference.GlobalValues.GameDateTime = inGameDateTime.Object;
+        }
 
+        [TestMethod]
+        public void GameStats_GenerateGameStats()
+        {
             string result = gameStats.GenerateGameStats();
             string expected = "\r\nUpTime: uptime\r\ntickTimes\r\nId:1  - zone1 - Rooms:1   - Mobs:1   - Items:1   - Players:1  \r\nId:20 - z2    - Rooms:100 - Mobs:100 - Items:100 - Players:100";
             Assert.IsTrue(result.Contains(expected));
