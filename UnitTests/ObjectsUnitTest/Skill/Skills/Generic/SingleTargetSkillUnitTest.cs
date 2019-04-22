c61e01ddf082d881958aced8b518ec04f27485ae
Tom Johnson <nolueram@gmail.com>
2019-04-22 06:23:27 -0400

    single target test

--- UnitTests/ObjectsUnitTest/Skill/Skills/Generic/SingleTargetSkillUnitTest.cs
+++ UnitTests/ObjectsUnitTest/Skill/Skills/Generic/SingleTargetSkillUnitTest.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Objects.Command.Interface;
+using Objects.Effect.Interface;
 using Objects.Global;
 using Objects.Global.FindObjects.Interface;
+using Objects.Global.StringManuplation.Interface;
 using Objects.Interface;
+using Objects.Language.Interface;
 using Objects.Mob.Interface;
 using Objects.Skill.Skills.Generic;
 using Shared.TagWrapper.Interface;
@@ -24,6 +27,9 @@ namespace ObjectsUnitTest.Skill.Skills.Generic
         Mock<IParameter> parameter0;
         Mock<IParameter> parameter1;
         Mock<ITagWrapper> tagWrapper;
+        Mock<IEffect> effect;
+        Mock<IStringManipulator> stringManipulator;
+        Mock<ITranslationMessage> translationMessage;
 
         [TestInitialize]
         public void Setup()
@@ -38,16 +44,27 @@ namespace ObjectsUnitTest.Skill.Skills.Generic
             parameter0 = new Mock<IParameter>();
             parameter1 = new Mock<IParameter>();
             tagWrapper = new Mock<ITagWrapper>();
+            effect = new Mock<IEffect>();
+            stringManipulator = new Mock<IStringManipulator>();
+            translationMessage = new Mock<ITranslationMessage>();
 
             singleTargetSkill.CallBase = true;
+            singleTargetSkill.Object.Effect = effect.Object;
+            singleTargetSkill.Object.PerformerNotificationSuccess = translationMessage.Object;
+            npc.Setup(e => e.SentenceDescription).Returns("npc");
+            npc2.Setup(e => e.SentenceDescription).Returns("npc2");
             parameter0.Setup(e => e.ParameterValue).Returns("param0");
             parameter1.Setup(e => e.ParameterValue).Returns("param1");
             findObjects.Setup(e => e.FindObjectOnPersonOrInRoom(npc.Object, parameter1.Object.ParameterValue, 0, true, true, true, true, true)).Returns(npc2.Object);
             command.Setup(e => e.Parameters).Returns(new List<IParameter>() { parameter0.Object, parameter1.Object });
             tagWrapper.Setup(e => e.WrapInTag(It.IsAny<string>(), TagType.Info)).Returns((string x, TagType y) => (x));
+            stringManipulator.Setup(e => e.UpdateTargetPerformer("npc", "npc2", "success")).Returns("updated");
+            translationMessage.Setup(e => e.GetTranslatedMessage(npc.Object)).Returns("success");
+
 
             GlobalReference.GlobalValues.FindObjects = findObjects.Object;
             GlobalReference.GlobalValues.TagWrapper = tagWrapper.Object;
+            GlobalReference.GlobalValues.StringManipulator = stringManipulator.Object;
         }
 
         [TestMethod]
@@ -74,8 +91,10 @@ namespace ObjectsUnitTest.Skill.Skills.Generic
         public void SingleTargetSkill_ProcessSkill_Found()
         {
             IResult result = singleTargetSkill.Object.ProcessSkill(npc.Object, command.Object);
-            Assert.AreEqual("You need 0 stamina to use the skill param0.", result.ResultMessage);
-            Assert.IsTrue(result.AllowAnotherCommand);
+            Assert.AreEqual("updated", result.ResultMessage);
+            Assert.IsFalse(result.AllowAnotherCommand);
+            effect.Verify(e => e.ProcessEffect(singleTargetSkill.Object.Parameter));
+            stringManipulator.Verify(e => e.UpdateTargetPerformer("npc", "npc2", "success"));
         }
     }
 }
