8dbc517bd13743bbcb3b2f562824ff7b1dab80d5
Tom Johnson <nolueram@gmail.com>
2019-05-24 06:42:29 -0400

    consider should be done

--- Objects/Command/PC/Consider.cs
+++ Objects/Command/PC/Consider.cs
@@ -1,4 +1,5 @@
 ﻿using Objects.Command.Interface;
+using Objects.Global;
 using Objects.Mob.Interface;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,23 @@ namespace Objects.Command.PC
 
         public IResult PerformCommand(IMobileObject performer, ICommand command)
         {
-            throw new NotImplementedException();
+            if (command.Parameters.Count <= 0)
+            {
+                return new Result("Who would you like to consider attacking?", true);
+            }
+
+            IParameter parm = command.Parameters[0];
+            IMobileObject mob = GlobalReference.GlobalValues.FindObjects.FindObjectOnPersonOrInRoom(performer, parm.ParameterValue, parm.ParameterNumber, false, false, true, true) as IMobileObject;
+
+            if (mob != null)
+            {
+                return new Result(GlobalReference.GlobalValues.EvaluateLevelDifference.Evalute(performer, mob), true);
+            }
+            else
+            {
+                string message = string.Format("You were unable to find {0}.", command.Parameters[0].ParameterValue);
+                return new Result(message, true);
+            }
         }
     }
 }
