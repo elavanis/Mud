93135e901e7a3231b040801d0fc678fef1736d87
Tom Johnson <tom.johnson@zirmed.com>
2019-07-02 15:56:01 -0400

    add mounts

---
+++ Objects/Mob/Interface/IMount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Objects.Mob.Interface
+{
+    public interface IMount : IMobileObject
+    {
+        IMobileObject Owner { get; set; }
+        int Movement { get; set; }
+        int StaminaMultiplier { get; set; }
+        SummonType SummonType { get; set; }
+        int MaxRiders { get; set; }
+        List<IMobileObject> Riders { get; set; }
+
+    }
+}
---
+++ Objects/Mob/Mount.cs
@@ -0,0 +1,24 @@
+﻿using Objects.Mob.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Objects.Mob
+{
+    public class Mount : MobileObject, IMount
+    {
+        public IMobileObject Owner { get; set; }
+        public int Movement { get; set; } = 2;
+        public int StaminaMultiplier { get; set; } = 10;
+        public SummonType SummonType { get; set; } = SummonType.Call;
+        public int MaxRiders { get; set; } = 1;
+        public List<IMobileObject> Riders { get; set; } = new List<IMobileObject>();
+    }
+
+    public enum SummonType
+    {
+        Summon,
+        Call,
+        None
+    }
+}
