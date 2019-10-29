c47b5965063f3d07b62518b4b0723bb917082ac2
Tom Johnson <tom.johnson@zirmed.com>
2019-10-29 10:32:17 -0400

    open enchantment

--- UnitTests/ObjectsUnitTest/Magic/Enchantment/OpenEnchantmentUnitTest.cs
+++ UnitTests/ObjectsUnitTest/Magic/Enchantment/OpenEnchantmentUnitTest.cs
@@ -1,5 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Objects.Effect.Interface;
 using Objects.Global;
+using Objects.Global.Random.Interface;
+using Objects.Item.Interface;
+using Objects.Magic.Enchantment;
+using Objects.Mob.Interface;
+using Objects.Room.Interface;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,17 +16,45 @@ namespace ObjectsUnitTest.Magic.Enchantment
     [TestClass]
     public class OpenEnchantmentUnitTest
     {
+        OpenEnchantment enchantment;
+        Mock<IEffect> effect;
+        Mock<IEffectParameter> parameter;
+        Mock<IRandom> random;
+        Mock<IMobileObject> mob;
+        Mock<IItem> item;
+        Mock<IRoom> room;
+
         [TestInitialize]
         public void Setup()
         {
             GlobalReference.GlobalValues = new GlobalValues();
 
+            enchantment = new OpenEnchantment();
+            effect = new Mock<IEffect>();
+            parameter = new Mock<IEffectParameter>();
+            random = new Mock<IRandom>();
+            mob = new Mock<IMobileObject>();
+            item = new Mock<IItem>();
+            room = new Mock<IRoom>();
+
+            enchantment.ActivationPercent = 100;
+            enchantment.Effect = effect.Object;
+            enchantment.Parameter = parameter.Object;
+            random.Setup(e => e.PercentDiceRoll(100)).Returns(true);
+            mob.Setup(e => e.Room).Returns(room.Object);
+
+            GlobalReference.GlobalValues.Random = random.Object;
         }
 
         [TestMethod]
         public void OpenEnchantmentUnitTest_WriteSome()
         {
-            Assert.AreEqual(1, 2);
+            enchantment.Open(mob.Object, item.Object);
+
+            effect.Verify(e => e.ProcessEffect(parameter.Object), Times.Once);
+            parameter.VerifySet(e => e.ObjectRoom = room.Object);
+            parameter.VerifySet(e => e.Item = item.Object);
+            parameter.VerifySet(e => e.Target = mob.Object);
         }
     }
 }
