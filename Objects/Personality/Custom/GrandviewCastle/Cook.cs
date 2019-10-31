3cfe32a7dbd6bc3a0ef9f00ef6139e5f09217a8c
Tom Johnson <tom.johnson@zirmed.com>
2019-10-31 14:27:51 -0400

    cook unit test

--- Objects/Personality/Custom/GrandviewCastle/Cook.cs
+++ Objects/Personality/Custom/GrandviewCastle/Cook.cs
@@ -9,133 +9,20 @@ namespace Objects.Personality.Custom.GrandviewCastle
 {
     public class Cook : IPersonality
     {
-        private State StateMachine { get; set; } = State.Wait;
-        private int Step;
 
         public string Process(INonPlayerCharacter npc, string command)
         {
-            Step++;
-
-            switch (StateMachine)
+            if (command == null)
             {
-                case State.Wait:
-                    CheckForTrigger(npc);
-                    break;
-                case State.AskedForHasenpfeffer:
-                    PerformSteps(npc);
-                    break;
-
-
+                CheckForTrigger(npc);
+                return null;
             }
-
-            return null;
-        }
-
-        private void PerformSteps(INonPlayerCharacter npc)
-        {
-            List<INonPlayerCharacter> nonPlayerCharacters = new List<INonPlayerCharacter>(npc.Room.NonPlayerCharacters);
-            for (int i = 0; i < nonPlayerCharacters.Count; i++)
+            else
             {
-                if (nonPlayerCharacters[i] == npc)
-                {
-                    switch (i)
-                    {
-                        case 0:
-                            switch (Step)
-                            {
-                                case 3:
-                                    npc.EnqueueCommand("Emote begins to cut up a rabbit.");
-                                    break;
-                                case 8:
-                                    npc.EnqueueCommand("Emote puts the rabbit in the pot.");
-                                    StateMachine = State.Wait;
-                                    break;
-                            }
-                            break;
-                        case 1:
-                            switch (Step)
-                            {
-                                case 3:
-                                    npc.EnqueueCommand("Emote begins to cut up a potatoes.");
-                                    break;
-                                case 6:
-                                    npc.EnqueueCommand("Emote puts the potatoes in the pot.");
-                                    StateMachine = State.Wait;
-                                    break;
-                            }
-                            break;
-                        case 2:
-                            switch (Step)
-                            {
-                                case 3:
-                                    npc.EnqueueCommand("Emote begins to cut up a carrots");
-                                    break;
-                                case 7:
-                                    npc.EnqueueCommand("Emote puts the carrots in the pot.");
-                                    StateMachine = State.Wait;
-                                    break;
-                            }
-                            break;
-                        case 3:
-                            switch (Step)
-                            {
-                                case 3:
-                                    npc.EnqueueCommand("Emote stokes the cooking fire.");
-                                    break;
-                                case 5:
-                                case 9:
-                                case 13:
-                                case 17:
-                                    npc.EnqueueCommand("Emote stirs the pot.");
-                                    break;
-                                case 19:
-                                    npc.EnqueueCommand("Say here you go.  Hasenpfeffer for the king.");
-                                    StateMachine = State.Wait;
-                                    break;
-                            }
-                            break;
-                    }
-                    break;
-                }
+                return command;
             }
         }
 
-        //private void CheckForTrigger(INonPlayerCharacter npc)
-        //{
-        //    string message = null;
-        //    while ((message = npc.DequeueMessage()) != null)
-        //    {
-        //        if (message == "<Communication>kings servant says The king wants hasenpfeffer to eat.</Communication>")
-        //        {
-        //            List<INonPlayerCharacter> nonPlayerCharacters = new List<INonPlayerCharacter>(npc.Room.NonPlayerCharacters);
-        //            for (int i = 0; i < nonPlayerCharacters.Count; i++)
-        //            {
-        //                if (nonPlayerCharacters[i] == npc)
-        //                {
-        //                    switch (i)
-        //                    {
-        //                        case 0:
-        //                            npc.EnqueueCommand("Say Right away");
-        //                            break;
-        //                        case 1:
-        //                            npc.EnqueueCommand("Say As the king wishes.");
-        //                            break;
-        //                        case 2:
-        //                            npc.EnqueueCommand("Say Sure.");
-        //                            break;
-        //                        case 3:
-        //                            npc.EnqueueCommand("Say As the king wishes.");
-        //                            break;
-        //                    }
-        //                    break;
-        //                }
-        //            }
-
-        //            StateMachine = State.AskedForHasenpfeffer;
-        //            Step = 0;
-        //        }
-        //    }
-        //}
 
         private static string CheckForTrigger(INonPlayerCharacter npc)
         {
@@ -153,7 +40,7 @@ namespace Objects.Personality.Custom.GrandviewCastle
                             switch (i)
                             {
                                 case 0:
-                                    npc.EnqueueCommand("Say Right away");
+                                    npc.EnqueueCommand("Say Right away.");
                                     EnqueueWait(npc, 2);
                                     npc.EnqueueCommand("Emote begins to cut up a rabbit.");
                                     EnqueueWait(npc, 5);
@@ -206,12 +93,6 @@ namespace Objects.Personality.Custom.GrandviewCastle
                 npc.EnqueueCommand("Wait");
 
             }
-        }
-
-        private enum State
-        {
-            Wait,
-            AskedForHasenpfeffer
         }
     }
 }
--- UnitTests/ObjectsUnitTest/Personality/Personalities/Custom/GrandviewCastle/CookUnitTest.cs
+++ UnitTests/ObjectsUnitTest/Personality/Personalities/Custom/GrandviewCastle/CookUnitTest.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using Objects.Global;
+using Objects.Mob.Interface;
+using Objects.Personality.Custom.GrandviewCastle;
+using Objects.Room.Interface;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace ObjectsUnitTest.Personality.Personalities.Custom.GrandviewCastle
@@ -9,17 +14,89 @@ namespace ObjectsUnitTest.Personality.Personalities.Custom.GrandviewCastle
     [TestClass]
     public class CookUnitTest
     {
+        Cook cook;
+        Mock<INonPlayerCharacter> npc;
+        Mock<IRoom> room;
+        Mock<INonPlayerCharacter> otherNpc1;
+        Mock<INonPlayerCharacter> otherNpc2;
+        Mock<INonPlayerCharacter> otherNpc3;
+        Mock<INonPlayerCharacter> otherNpc4;
+        Mock<INonPlayerCharacter> otherNpc5;
+
         [TestInitialize]
         public void Setup()
         {
             GlobalReference.GlobalValues = new GlobalValues();
 
+            npc = new Mock<INonPlayerCharacter>();
+            room = new Mock<IRoom>();
+            otherNpc1 = new Mock<INonPlayerCharacter>();
+            otherNpc2 = new Mock<INonPlayerCharacter>();
+            otherNpc3 = new Mock<INonPlayerCharacter>();
+            otherNpc4 = new Mock<INonPlayerCharacter>();
+            otherNpc5 = new Mock<INonPlayerCharacter>();
+
+            npc.Setup(e => e.Room).Returns(room.Object);
+            otherNpc1.Setup(e => e.Room).Returns(room.Object);
+            otherNpc2.Setup(e => e.Room).Returns(room.Object);
+            otherNpc3.Setup(e => e.Room).Returns(room.Object);
+            otherNpc4.Setup(e => e.Room).Returns(room.Object);
+            otherNpc5.Setup(e => e.Room).Returns(room.Object);
+            room.Setup(e => e.NonPlayerCharacters).Returns(new List<INonPlayerCharacter>() { otherNpc1.Object, otherNpc2.Object, otherNpc3.Object, otherNpc4.Object, otherNpc5.Object, npc.Object });
+
+            cook = new Cook();
+
+
+            //PropertyInfo stateMachine = cook.GetType().GetProperty("StateMachine", BindingFlags.Instance | BindingFlags.NonPublic);
+            //object state = cook.GetType().GetNestedType("State", BindingFlags.NonPublic).GetField("AskedForHasenpfeffer").GetValue(cook);
+            //stateMachine.SetValue(cook, state);
+
         }
 
         [TestMethod]
-        public void CookUnitTest_WriteSome()
+        public void Cook_Process_CommandNotNull()
         {
-            Assert.AreEqual(1, 2);
+            string command = "original";
+
+            Assert.AreSame(command, cook.Process(npc.Object, command));
+        }
+
+        [TestMethod]
+        public void Cook_Process_CheckForTrigger()
+        {
+            otherNpc1.SetupSequence(e => e.DequeueMessage()).Returns("<Communication>kings servant says The king wants hasenpfeffer to eat.</Communication>");
+            otherNpc2.SetupSequence(e => e.DequeueMessage()).Returns("<Communication>kings servant says The king wants hasenpfeffer to eat.</Communication>");
+            otherNpc3.SetupSequence(e => e.DequeueMessage()).Returns("<Communication>kings servant says The king wants hasenpfeffer to eat.</Communication>");
+            otherNpc4.SetupSequence(e => e.DequeueMessage()).Returns("<Communication>kings servant says The king wants hasenpfeffer to eat.</Communication>");
+            otherNpc5.SetupSequence(e => e.DequeueMessage()).Returns("<Communication>kings servant says The king wants hasenpfeffer to eat.</Communication>");
+
+            cook.Process(otherNpc1.Object, null);
+            otherNpc1.Verify(e => e.EnqueueCommand("Wait"), Times.Exactly(7));
+            otherNpc1.Verify(e => e.EnqueueCommand("Say Right away."), Times.Once);
+            otherNpc1.Verify(e => e.EnqueueCommand("Emote begins to cut up a rabbit."), Times.Once);
+            otherNpc1.Verify(e => e.EnqueueCommand("Emote puts the rabbit in the pot."), Times.Once);
+
+            cook.Process(otherNpc2.Object, null);
+            otherNpc2.Verify(e => e.EnqueueCommand("Wait"), Times.Exactly(5));
+            otherNpc2.Verify(e => e.EnqueueCommand("Say As the king wishes."), Times.Once);
+            otherNpc2.Verify(e => e.EnqueueCommand("Emote begins to cut up a potatoes."), Times.Once);
+            otherNpc2.Verify(e => e.EnqueueCommand("Emote puts the potatoes in the pot."), Times.Once);
+
+            cook.Process(otherNpc3.Object, null);
+            otherNpc3.Verify(e => e.EnqueueCommand("Wait"), Times.Exactly(6));
+            otherNpc3.Verify(e => e.EnqueueCommand("Say Sure."), Times.Once);
+            otherNpc3.Verify(e => e.EnqueueCommand("Emote begins to cut up a carrots."), Times.Once);
+            otherNpc3.Verify(e => e.EnqueueCommand("Emote puts the carrots in the pot."), Times.Once);
+
+            cook.Process(otherNpc4.Object, null);
+            otherNpc4.Verify(e => e.EnqueueCommand("Wait"), Times.Exactly(20));
+            otherNpc4.Verify(e => e.EnqueueCommand("Say As the king wishes."), Times.Once);
+            otherNpc4.Verify(e => e.EnqueueCommand("Emote stokes the cooking fire."), Times.Once);
+            otherNpc4.Verify(e => e.EnqueueCommand("Emote stirs the pot."), Times.Exactly(4));
+            otherNpc4.Verify(e => e.EnqueueCommand("Say here you go.  Hasenpfeffer for the king."), Times.Once);
+
+            otherNpc5.Verify(e => e.EnqueueCommand(It.IsAny<string>()), Times.Never);
+
         }
     }
 }
