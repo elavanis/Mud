7373c3826519c495bc529f79c413ae16e91d5b80
Tom Johnson <tom.johnson@zirmed.com>
2019-12-19 14:29:36 -0500

    starting close command

--- Misc/GenerateZones/Zones/UnderGrandView/UnderGrandViewCastle.cs
+++ Misc/GenerateZones/Zones/UnderGrandView/UnderGrandViewCastle.cs
@@ -28,62 +28,7 @@ namespace GenerateZones.Zones.UnderGrandView
         {
         }
 
-        //        public IZone Generate()
-        //        {
-        //            Zone.Name = nameof(UnderGrandViewCastle);
 
-        //            BuildRoomsViaReflection(this.GetType());
-
-        //            ConnectRooms();
-
-        //            return Zone;
-        //        }
-
-
-
-        //        #region Rooms
-        //        private IRoom GenerateRoom1()
-        //        {
-        //            IRoom room = IndoorRoomNoLight();
-
-        //            room.ExamineDescription = "Dirt from above still occasionally falls down and one day may fill the tunnel.";
-        //            room.LookDescription = "The tunnel was originally part of an underground sink hole network.";
-        //            room.ShortDescription = "Underground cavern";
-
-        //            return room;
-        //        }
-
-        //        private IRoom GenerateRoom2()
-        //        {
-        //            IRoom room = IndoorRoomNoLight();
-
-        //            room.ExamineDescription = "The dirt from above has filled most of this cavern.  Several small tunnels lead off but most are impassable beyond a few feet.";
-        //            room.LookDescription = "A large mound of dirt fills the cavern.";
-        //            room.ShortDescription = "Underground cavern";
-
-        //            return room;
-        //        }
-
-        //        private IRoom GenerateRoom3()
-        //        {
-        //            IRoom room = IndoorRoomNoLight();
-
-        //            room.ExamineDescription = "The tunnel to the west appears to be natural or not finished while the tunnel to the east is lined with bricks.";
-        //            room.LookDescription = "This point in the tunnel transitions between a rough natural tunnel and a brick lined tunnel with torches.";
-        //            room.ShortDescription = "Underground cavern";
-
-        //            return room;
-        //        }
-
-        //        #endregion Rooms
-
-        //        private void ConnectRooms()
-        //        {
-        //            ZoneHelper.ConnectRoom(Zone.Rooms[1], Direction.East, Zone.Rooms[2]);
-        //            ZoneHelper.ConnectRoom(Zone.Rooms[2], Direction.East, Zone.Rooms[3]);
-        //        }
-        //    }
-        //}
 
 
         public IZone Generate()
---
+++ Objects/Command/PC/Close.cs
@@ -0,0 +1,21 @@
+﻿using Objects.Command.Interface;
+using Objects.Mob.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Objects.Command.PC
+{
+    public class Close : IMobileObjectCommand
+    {
+        public IResult Instructions { get; } = new Result("Close [Item Name]", true);
+
+        public IEnumerable<string> CommandTrigger { get; } = new List<string>() { "Close" };
+
+
+        public IResult PerformCommand(IMobileObject performer, ICommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
